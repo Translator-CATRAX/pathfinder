f9165e2d7115cd9d3d55c05fb55c8abe297c4f26
mohsenht <mohsen.taheri.1989@gmail.com>
2026-06-24 12:44:27 -0400

    Faster PathRanker plus Three hops pathfinder added

---
+++ src/pathfinder/converter/EdgeExtractorFromTRAPIResponse.py
@@ -0,0 +1,45 @@
+class EdgeExtractorFromTRAPIResponse:
+
+    def __init__(self, trapi_response, logger):
+        self.logger = logger
+        self.pairs_to_edge_ids = {}
+        self.knowledge_graph = trapi_response["message"]["knowledge_graph"]
+        for edge_id, info in trapi_response["message"]["knowledge_graph"]["edges"].items():
+            edge_key_1 = f"{info["object"]}--{info["subject"]}"
+            edge_key_2 = f"{info["subject"]}--{info["object"]}"
+            if self.pairs_to_edge_ids.get(edge_key_1) is None and self.pairs_to_edge_ids.get(edge_key_2) is None:
+                self.pairs_to_edge_ids[edge_key_1] = [edge_id]
+            elif self.pairs_to_edge_ids.get(edge_key_1) is None:
+                self.pairs_to_edge_ids[edge_key_2].append(edge_id)
+            else:
+                self.pairs_to_edge_ids[edge_key_1].append(edge_id)
+        logger.info(f"Number of edges cached: {len(self.pairs_to_edge_ids)}")
+
+    def get_edges(self, pairs, logger):
+        cached_pairs = []
+        i = 0
+        while i < len(pairs):
+            edge_key_1 = f"{pairs[i][0]}--{pairs[i][1]}"
+            edge_key_2 = f"{pairs[i][1]}--{pairs[i][0]}"
+            if edge_key_1 in self.pairs_to_edge_ids:
+                cached_pairs.append(edge_key_1)
+                del pairs[i]
+            elif edge_key_2 in self.pairs_to_edge_ids:
+                cached_pairs.append(edge_key_2)
+                del pairs[i]
+            else:
+                i += 1
+        if len(pairs) != 0:
+            logger.error(f"Cannot retrieve {pairs} from trapi response.")
+        knowledge_graph = {'edges': {}, 'nodes': {}}
+        for cached_pair in cached_pairs:
+            list_of_edges = self.pairs_to_edge_ids[cached_pair]
+            cached_nodes = cached_pair.split("--")
+            if cached_nodes[0] in self.knowledge_graph['nodes']:
+                knowledge_graph['nodes'][cached_nodes[0]] = self.knowledge_graph['nodes'][cached_nodes[0]]
+            if cached_nodes[1] in self.knowledge_graph['nodes']:
+                knowledge_graph['nodes'][cached_nodes[1]] = self.knowledge_graph['nodes'][cached_nodes[1]]
+            for edge in list_of_edges:
+                if str(edge) in self.knowledge_graph['edges']:
+                    knowledge_graph['edges'][str(edge)] = self.knowledge_graph['edges'][str(edge)]
+        return knowledge_graph
---
+++ src/tests/test_explore_path_finder.py
@@ -0,0 +1,96 @@
+import json
+import logging
+import time
+from pathlib import Path
+
+from pathfinder.Pathfinder import Pathfinder
+
+HERE = Path(__file__).parent
+
+
+def test_pathfinder():
+    src_node_id = "CHEBI:45783"
+    dst_node_id = "MONDO:0004979"
+    src_pinned_node = "n1"
+    dst_pinned_node = "n2"
+    pathfinder_request = {
+        "message": {
+            "auxiliary_graphs": {},
+            "knowledge_graph": {
+                "edges": {},
+                "nodes": {}
+            },
+            "query_graph": {
+                "nodes": {
+                    src_pinned_node: {
+                        "ids": [
+                            src_node_id
+                        ]
+                    },
+                    dst_pinned_node: {
+                        "ids": [
+                            dst_node_id
+                        ]
+                    }
+                },
+                "paths": {
+                    "p0": {
+                        "object": dst_pinned_node,
+                        "subject": src_pinned_node
+                    }
+                }
+            },
+            "results": []
+        }
+    }
+
+    HERE = Path(__file__).parent
+
+    logger = logging.getLogger("tests.pathfinder")
+    logger.setLevel(logging.INFO)
+
+    ngd_path = HERE / "../../curie_ngd_v1.0_tier0-20260408.sqlite"
+    kg2c_path = HERE / "../../tier0-info-for-overlay_v1.0_tier0-20260408.sqlite"
+    pathfinder = Pathfinder(
+        "MLRepo",
+        f"gandalf:{HERE / '../../gandalf_mmap'}",
+        f"sqlite:{ngd_path}",
+        f"sqlite:{kg2c_path}",
+        {},
+        {},
+        logger
+    )
+    start_time = time.perf_counter()
+    result, aux_graphs, knowledge_graph = pathfinder.get_paths(
+        src_node_id,
+        dst_node_id,
+        src_pinned_node,
+        dst_pinned_node,
+        4,
+        4,
+        500,
+        75,
+        30000,
+        None
+    )
+    end_time = time.perf_counter()
+    execution_time = end_time - start_time
+    print(f"Executed in {execution_time:.6f} seconds")
+    res = []
+    if result is not None:
+        res.append(
+            {
+                "id": result["id"],
+                "analyses": result["analyses"],
+                "node_bindings": result["node_bindings"],
+                "essence": "result",
+            }
+        )
+    if aux_graphs is None:
+        aux_graphs = {}
+    if knowledge_graph is None:
+        knowledge_graph = {}
+    pathfinder_request["message"]["knowledge_graph"] = knowledge_graph
+    pathfinder_request["message"]["auxiliary_graphs"] = aux_graphs
+    pathfinder_request["message"]["results"] = res
+    json.dump(pathfinder_request, open(HERE / "path_response.json", "w"))
