a9583794bfebb23a3a642baeb23e4467bbb1f9c5
mohsenht <mohsen.taheri.1989@gmail.com>
2026-08-14 13:20:59 -0400

    add timings

--- src/pathfinder/converter/EdgeExtractorFromTRAPIResponse.py
+++ src/pathfinder/converter/EdgeExtractorFromTRAPIResponse.py
@@ -5,8 +5,8 @@ class EdgeExtractorFromTRAPIResponse:
         self.pairs_to_edge_ids = {}
         self.knowledge_graph = knowledge_graph
         for edge_id, info in knowledge_graph["edges"].items():
-            edge_key_1 = f"{info["object"]}--{info["subject"]}"
-            edge_key_2 = f"{info["subject"]}--{info["object"]}"
+            edge_key_1 = f"{info['object']}--{info['subject']}"
+            edge_key_2 = f"{info['subject']}--{info['object']}"
             if self.pairs_to_edge_ids.get(edge_key_1) is None and self.pairs_to_edge_ids.get(edge_key_2) is None:
                 self.pairs_to_edge_ids[edge_key_1] = [edge_id]
             elif self.pairs_to_edge_ids.get(edge_key_1) is None:
